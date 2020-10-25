93d40171be933c9011b1619d8c1a1379ba6da852
Darek Kay <darekkay@gmail.com>
2020-10-25 10:33:47 +0100

    Update dependencies, add new rules

--- plugins/jest.js
+++ plugins/jest.js
@@ -24,6 +24,7 @@ module.exports = {
     "jest/no-conditional-expect": "error",
     "jest/no-deprecated-functions": "error",
     "jest/no-disabled-tests": "off",
+    "jest/no-done-callback": "error",
     "jest/no-duplicate-hooks": "error",
     "jest/no-export": "error",
     "jest/no-focused-tests": "error",
