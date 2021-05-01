6b70365ab14393464200877f14981082804312eb
Darek Kay <hello@darekkay.com>
2021-05-01 22:11:39 +0200

    Don't lint the babel config file by default

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-  IGNORE_PATTERNS: [".eslintrc.js", "jest.config.js"],
+  IGNORE_PATTERNS: [".eslintrc.js", "babel.config.js", "jest.config.js"],
 
   FILE_PATTERN_TESTING: [
     "**/__tests__/**/*.[jt]s?(x)",
