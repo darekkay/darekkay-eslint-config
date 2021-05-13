c457558d2cac14ae03f87fe0abd3fd5b4833e549
Darek Kay <hello@darekkay.com>
2021-05-13 11:14:34 +0200

    Check all plugins for new rules
    Overrides are not supported, yet, so some rules were missed. See: https://github.com/sarbbottam/eslint-find-rules/issues/317

--- internals/config-all.js
+++ internals/config-all.js
@@ -1,3 +1,19 @@
 module.exports = {
-  extends: ["../react", "../nodejs", "../typescript"],
+  extends: [
+    "../base.js",
+
+    "../plugins/import.js",
+    "../plugins/jest.js",
+    "../plugins/jest-dom.js",
+    "../plugins/node.js",
+    "../plugins/react.js",
+    "../plugins/testing-library.js",
+    "../plugins/typescript-static.js",
+    "../plugins/typescript-types.js",
+    "../plugins/unicorn.js",
+
+    "../plugins/prettier.js",
+  ],
+
+  plugins: ["@typescript-eslint"],
 };
