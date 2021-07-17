dcc9a0e32ad431336cc773bf97c2c7fc77edac90
Darek Kay <hello@darekkay.com>
2021-07-17 14:11:01 +0200

    Update dependencies and rules

--- plugins/testing-library.js
+++ plugins/testing-library.js
@@ -29,6 +29,7 @@ module.exports = {
     "testing-library/prefer-explicit-assert": "error",
     "testing-library/prefer-find-by": "error",
     "testing-library/prefer-presence-queries": "error",
+    "testing-library/prefer-query-by-disappearance": "error",
     "testing-library/prefer-screen-queries": "error",
     "testing-library/prefer-user-event": "error",
     "testing-library/prefer-wait-for": "error",
